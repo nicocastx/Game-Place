7873e4afa864649b748e6e17ec0e29240889cc0d
Kevin Nicolas <nicocastx@gmail.com>
2022-08-22 13:19:39 -0300

    2do desafio REACT menu e-commerce en un componente

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//ordenar todo en una carpeta components
+import App from './components/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
