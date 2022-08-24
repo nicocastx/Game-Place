d94648e443c0b86b9d7bec0fd581a185627f48c8
Kevin Nicolas <nicocastx@gmail.com>
2022-08-24 20:40:02 -0300

    3er Desafio correcion coder react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Nav/>
-      <ItemListContainer/>
+      <ItemListContainer mensaje="Bienvenido a Game Place"/>
     </div>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './ItemListContainer.css';
 
-export default function ItemListContainer() {
+export default function ItemListContainer({mensaje}) {
     return (
         <div className = "ItemListContainer">
-            <h1>Bienvenido a Game Place</h1>
+            <h1>{mensaje}</h1>
         </div>
     )
 }
