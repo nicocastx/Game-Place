4e4161aa0b42241f3b8df6b2b50577c48f91361c
Kevin Castillo <nicocastx2@gmail.com>
2022-08-29 19:04:22 -0300

    Entrega desafio contador con boton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import "./App.css";
 import Nav from "./navBar/Nav";
 import ItemListContainer from './itemListContainer/ItemListContainer';
+import Card from './cards/Card';
 
 function App() {
+  const juego1 = {nombre: 'Minecraft', descripcion: 'Hola xd lorem ipsum', stock: 8}
   return (
     <div className="App">
       <Nav/>
       <ItemListContainer mensaje="Bienvenido a Game Place"/>
+      <Card juego={juego1}/>
     </div>
   );
 }
